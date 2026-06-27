c95c20c8d23ff059602e7ebc121a2780bd90dc40
Gene Hawkins <texxasrulez@yahoo.com>
2026-06-27 12:46:14 -0500

    Update scheduled_sending.php
    version bump

--- scheduled_sending.php
+++ scheduled_sending.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/queue.inc.php';
  */
 class scheduled_sending extends rcube_plugin
 {
-    const PLUGIN_VERSION = '1.3.0';
+    const PLUGIN_VERSION = '1.3.1';
     const PLUGIN_INFO = array(
         'name' => 'scheduled_sending',
         'vendor' => 'Gene Hawkins',
